34d1ecb4b9174b4e7847fd6f7ad83d890dc437cf
Matrix <immatrixchan@gmail.com>
2021-07-28 17:58:04 +0800

    part1-b:步骤4 课程信息

--- part1/exercises/courseinfo/src/App.js
+++ part1/exercises/courseinfo/src/App.js
@@ -9,7 +9,6 @@ const Header = (props)=>{
 }
 
 const Part = (props)=>{
-  console.log(props)
   const part = props.part
   return(
     <p>{part.name} {part.exercises}</p>
@@ -17,12 +16,15 @@ const Part = (props)=>{
 }
 
 const Content = (props)=>{
-  return(
-    <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+  const parts = props.parts
 
+
+  return(
+
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </div>
   )
 }
@@ -39,28 +41,27 @@ const Total = (props)=>{
 const App = ()=>{
  
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
 
   return(
     <div>
       <Header course={course} />
-      <Content 
-        part1={part1} 
-        part2={part2} 
-        part3={part3}
-      />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
